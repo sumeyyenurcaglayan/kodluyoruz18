40820f671c06d76232bb8f7ee84a38f888c5dc6d
Sumeyye Nur <nurcaglayan@gmail.com>
2021-09-19 02:55:23 +0300

    second

---
+++ src/day-1/1-lexical-structure.js
@@ -0,0 +1,7 @@
+let a = 123;
+a = "Kodluyoruz";
+a = 3333;
+
+console.log(typeof a);
+console.log("odad");
+alert("jhkjh");
--- src/day-1/2-objects.js
+++ src/day-1/2-objects.js
@@ -1,2 +1,39 @@
-//aa
-//ajdkhaksjhaksf
+const araba = {
+  color: 0,
+  brand: "Renault",
+  length: 5.3,
+  isDriving: false,
+  wheelCount: 4,
+  drive: function () {
+    this.isDriving = true;
+    console.log("I drive!", this.isDriving);
+  },
+  otherSpec: {
+    year: 2016,
+  },
+};
+
+araba.drive(araba);
+
+araba.brand = "Mercedes";
+
+console.log(araba.brand);
+console.log(araba["brand"]);
+
+console.log(araba.color);
+
+console.log(araba?.lights?.light1);
+
+if (araba.lights) {
+  console.log(araga.lights.light1);
+}
+
+console.log(
+  araba.brand == "Mercedes"
+    ? console.log("Evet o Mercedes")
+    : console.log("Hayır o Renault")
+);
+
+delete araba.color;
+
+console.log(araba.color);
